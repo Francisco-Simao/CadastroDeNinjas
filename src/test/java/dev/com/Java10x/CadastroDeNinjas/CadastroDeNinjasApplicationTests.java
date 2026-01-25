f225a5ae50345cd72509dc8725b1b565b7d4b56e
FRANCISCO RIBEIRO SIMAO <franciscoribeirosimao2000@gmail.com>
2026-01-25 10:49:15 -0300

    primeiro commit

---
+++ src/test/java/dev/com/Java10x/CadastroDeNinjas/CadastroDeNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.com.Java10x.CadastroDeNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
