d268de550b09e23a8e0ba6fc0f83367e9fb307f0
FRANCISCO RIBEIRO SIMAO <franciscoribeirosimao2000@gmail.com>
2026-01-26 10:38:08 -0300

    Criado a controller [Aula 2]

---
+++ src/main/java/dev/com/Java10x/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.com.Java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Hello Word , esta minha primeiras mensagem da rota";
+    }
+}
